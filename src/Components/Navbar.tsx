d125f211eab0082b315f716a6920eb2eaeb52ab0
gonnnzadev <gonnnzadev@gonnnzapc.gonza>
2026-07-12 03:12:43 -0300

    mejoras de css y mejore en general el diseño

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,5 @@
 import './Navbar.css'
-import main from'../assets/main.png'
+import main from '../assets/main.png'
 import { Link } from "react-router-dom";
 
 
@@ -10,7 +10,9 @@ export default function Navbar() {
             <nav className='navbar-container'>
                 <ul className='navbar__list'>
                     <li className='navbar__list-item'>
-                        <Link to="/projects">Projects</Link>
+                        <Link to="/projects">
+                            <svg fill="#ffffff" height="40px" width="40px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <polygon points="8,4 8,5 9,5 9,6 16,6 16,7 17,7 17,8 19,8 19,5 18,5 18,4 11,4 11,3 10,3 10,2 3,2 3,3 2,3 2,15 4,15 4,5 5,5 5,4 "></polygon> <path d="M21,11v-1h-6V9h-1V8H7v1H6v12h1v1h14v-1h1V11H21z M20,19h-1v1H9v-1H8v-8h1v-1h3v1h1v1h6v1h1V19z"></path> </g> </g></svg>
+                        </Link>
 
                     </li>
 
@@ -21,13 +23,16 @@ export default function Navbar() {
                     </li>
                     <li className='navbar__list-item'>
 
-                        <Link to="/about-me">AboutMe</Link>
+                        <Link to="/about-me">
+                            <svg fill="#ffffff" height="40px" width="40px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M6,12h1v1h6v-1h1v-1h1V5h-1V4h-1V3H7v1H6v1H5v6h1V12z M7,7h1V6h1V5h2v1h1v1h1v2h-1v1h-1v1H9v-1H8V9H7V7z"></path> <polygon points="16,15 15,15 15,14 4,14 4,15 3,15 3,16 2,16 2,21 4,21 4,18 5,18 5,17 6,17 6,16 13,16 13,17 14,17 14,18 15,18 15,21 17,21 17,16 16,16 "></polygon> <polygon points="18,12 19,12 19,11 20,11 20,5 19,5 19,4 18,4 18,3 17,3 16,3 16,6 17,6 17,7 18,7 18,8 18,9 17,9 17,10 16,10 16,13 18,13 "></polygon> <polygon points="21,16 21,15 20,15 20,14 18,14 18,17 19,17 19,18 20,18 20,21 22,21 22,16 "></polygon> </g> </g></svg>
+
+                        </Link>
 
                     </li>
-                    
+
                 </ul>
             </nav>
-        
+
         </>
 
     );
